859c25fbd4bcee221b18a785158896d5f0133cca
RobinWM <v517649974769@gmail.com>
2025-03-03 11:17:05 +0800

    feat: update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -149,7 +149,16 @@ const Nav: DefaultTheme.NavItem[] = [
     link: '/about/'
   },
   { text: '交互式文档', link: `${ogUrl}interactive/`, target: '_blank' },
-  { text: '演练场', link: `${ogUrl}play/`, target: '_blank' }
+  { text: '演练场', link: `${ogUrl}play/`, target: '_blank' },
+  {
+    text: '友情链接',
+    items: [
+      {
+        link: 'https://gokuai.top/',
+        text: 'Goku AI Video Generator'
+      }
+    ]
+  }
   // {
   //   text: `v${version}`,
   //   items: [
